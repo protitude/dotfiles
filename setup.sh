c9c3734a70ca465774587874a81d1a305bfa659b
Miles France <miles.france@gmail.com>
2016-02-27 23:11:29 -0700

    set all symlinks

--- setup.sh
+++ setup.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
-if [ ! -e "~/.bash_profile" ]
+if [ ! -L ~/.bash_profile ]
 then
+  ln -s $(pwd)"/.bash_profile" ~/.bash_profile
   echo "linked .bash_profile"
+fi
+
+if [ ! -L ~/.bash_ps1 ]
+then
+  ln -s $(pwd)"/.bash_ps1" ~/.bash_ps1
+  echo "linked .bash_ps1"
+fi
+
+if [ ! -L ~/.vim ]
+then
+  ln -s $(pwd)"/.vim" ~/.vim
+  echo "linked .vim"
+fi
+
+if [ ! -L ~/.vimrc ]
+then
+  ln -s $(pwd)"/.vimrc" ~/.vimrc
+  echo "linked .vimrc"
+fi
+
+if [ ! -L ~/.osx ]
+then
+  ln -s $(pwd)"/.osx" ~/.osx
+  echo "linked .osx"
 fi
